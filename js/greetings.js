824004890da36307e3c2b178efa51939e23522b2
Liam S C <contact@liamsc.net>
2021-10-29 22:09:09 +0100

    should be done? ]

--- js/greetings.js
+++ js/greetings.js
@@ -3,13 +3,13 @@ var myDate = new Date();
 
     var greet;
 
-    if (hrs < 12 && hrs >= 5)
+    if (hrs < 12 && hrs >= 4)
         greet = 'Good Morning';
     else if (hrs >= 12 && hrs <= 17)
         greet = 'Good Afternoon';
-    else if (hrs >= 17 && hrs <= 24)
+    else if (hrs >= 17 && hrs <= 20)
         greet = 'Good Evening';
-    else if (hrs >= 0 && hrs <= 5)
+    else
         greet = 'Good Night';
 
     document.getElementById('lblGreetings').innerHTML =
