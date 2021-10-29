fd55d20b8ceef70972dad763204fd851e93f0a64
Liam S C <contact@liamsc.net>
2021-10-29 22:12:17 +0100

    lol

--- js/dist/greetings.dev.js
+++ js/dist/greetings.dev.js
@@ -3,5 +3,5 @@
 var myDate = new Date();
 var hrs = myDate.getHours();
 var greet;
-if (hrs < 12 && hrs >= 4) greet = 'Good Morning';else if (hrs >= 12 && hrs <= 17) greet = 'Good Afternoon';else if (hrs >= 17 && hrs <= 20) greet = 'Good Evening';else greet = 'Good Night';
+if (hrs < 12 && hrs >= 4) greet = 'Good Morning';else if (hrs >= 12 && hrs < 17) greet = 'Good Afternoon';else if (hrs >= 17 && hrs < 20) greet = 'Good Evening';else greet = 'Good Night';
 document.getElementById('lblGreetings').innerHTML = '<b>' + greet + '</b>!';
--- js/greetings.js
+++ js/greetings.js
@@ -5,9 +5,9 @@ var myDate = new Date();
 
     if (hrs < 12 && hrs >= 4)
         greet = 'Good Morning';
-    else if (hrs >= 12 && hrs <= 17)
+    else if (hrs >= 12 && hrs < 17)
         greet = 'Good Afternoon';
-    else if (hrs >= 17 && hrs <= 20)
+    else if (hrs >= 17 && hrs < 20)
         greet = 'Good Evening';
     else
         greet = 'Good Night';
